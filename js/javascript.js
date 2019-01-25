eb3946bd8d224b4215522f975390a0ecec228841
Emre Kaynak <emrekaynak94@gmail.com>
2019-01-24 22:44:25 -0800

    Added prototype event listener for selector

--- js/javascript.js
+++ js/javascript.js
@@ -1,1 +1,13 @@
-console.log("Can you hear me");
+$(function() {
+  const apiUrl = {
+    linkStart: "https://api.nytimes.com/svc/topstories/v2/",
+    linkEnd: ".json?api-key=",
+    apiKey: "NCDiZQkQNXxf2tNaiVx4oiQBxNeGjpx9"
+  };
+  //Selector Event Listener
+  $(".selector-container").on("change", "#selector", function() {
+    console.log(
+      apiUrl.linkStart + $(this).val() + apiUrl.linkEnd + apiUrl.apiKey
+    );
+  });
+});
