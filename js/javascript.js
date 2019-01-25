95f6b798a7555b58b20f7da4ee6689f0d99e2708
Emre Kaynak <emrekaynak94@gmail.com>
2019-01-25 01:32:44 -0800

    JS code to generate proper li elements added

--- js/javascript.js
+++ js/javascript.js
@@ -1,7 +1,6 @@
 $(function() {
   //variables
   const errorMsg = "Couldn't load news";
-  let firstSelection = true;
   //objects
   const apiUrl = {
       linkStart: "https://api.nytimes.com/svc/topstories/v2/",
@@ -27,10 +26,7 @@ $(function() {
     })
       .done(function(data) {
         //Api data request Success
-        console.log(
-          apiUrl.linkStart + section + apiUrl.linkEnd + apiUrl.apiKey
-        );
-        console.log(data);
+        processResults(data.results); //Executes necessary instructions after success
       })
       .fail(function() {
         //Api data request Fail
@@ -43,6 +39,32 @@ $(function() {
         $("#loading").addClass("loaded");
       });
   }
+
+  function processResults(results) {
+    $.each(results, function(key, value) {
+      //loops through results
+      if (value.multimedia[0] !== undefined) {
+        //selects stories with images
+        $(".article-list").append(
+          storyHtml(value.url, value.multimedia[0].url, value.abstract) //generates the necessary html code for the li element
+        );
+      }
+    });
+  }
+
+  function storyHtml(link, img, text) {
+    //generates html code for a li element
+    return (
+      "<li class='story'><a href=" +
+      link +
+      "><img src=" +
+      img +
+      " alt='article picture'><p class='abstract'>" +
+      text +
+      "</p></a></li>"
+    );
+  }
+
   // MAIN CODE
   //Selector Event Listener
   $(".selector-container").on("change", "#selector", function() {
