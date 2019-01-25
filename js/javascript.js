0fee4a45e98b69b587b4d580569750d835badc34
Emre Kaynak <emrekaynak94@gmail.com>
2019-01-24 22:57:23 -0800

    Api Request script added

--- js/javascript.js
+++ js/javascript.js
@@ -1,13 +1,32 @@
 $(function() {
+  //variables
+  //objects
   const apiUrl = {
     linkStart: "https://api.nytimes.com/svc/topstories/v2/",
     linkEnd: ".json?api-key=",
     apiKey: "NCDiZQkQNXxf2tNaiVx4oiQBxNeGjpx9"
   };
+  //functions
+
+  // MAIN CODE
   //Selector Event Listener
   $(".selector-container").on("change", "#selector", function() {
-    console.log(
-      apiUrl.linkStart + $(this).val() + apiUrl.linkEnd + apiUrl.apiKey
-    );
+    const section = $(this).val();
+    //Add loading img
+    //Api Data Request
+    $.ajax({
+      method: "GET",
+      url: apiUrl.linkStart + section + apiUrl.linkEnd + apiUrl.apiKey,
+      dataType: "json"
+    })
+      .done(function(data) {
+        //Api data request Success
+        console.log(
+          apiUrl.linkStart + section + apiUrl.linkEnd + apiUrl.apiKey
+        );
+        console.log(data);
+      })
+      .fail() //Api data request Fail
+      .always(); //Remove loading img
   });
 });
